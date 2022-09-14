6e438587b9722724ac0226776fcf6efea318d0a3
SelimBahcivan <y.selimbahcivan@gmail.com>
2022-09-14 17:28:12 +0300

    - Added ChangeDetails Post Method - Added UserPasswordChangeDto - Added PasswordChange Get/Post Method - Added PasswordChange View

---
+++ Entities/DTOs/UserPasswordChangeDto.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.DTOs
+{
+    public class UserPasswordChangeDto
+    {
+        [DisplayName("Şu Anki Şifreniz")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        public string CurrentPassword { get; set; }
+
+        [DisplayName("Yeni Şifreniz")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        public string NewPassword { get; set; }
+
+        [DisplayName("Yeni Şifrenizin Tekrarı")]
+        [Required(ErrorMessage = "{0} boş geçilmemelidir.")]
+        [MaxLength(30, ErrorMessage = "{0} {1} karakterden büyük olmamalıdır.")]
+        [MinLength(5, ErrorMessage = "{0} {1} karakterden küçük olmamalıdır.")]
+        [DataType(DataType.Password)]
+        [Compare("NewPassword", ErrorMessage = "Girmiş olduğunuz Yeni Şifreniz ile Yeni Şifreniz alanları birbiriyle uyuşmalıdır.")]
+        public string RepeatPassword { get; set; }
+    }
+}
